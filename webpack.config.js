c77a7665f319e4bdc949af121c3b908f4975b95d
atsuya <98314945+atsuya-td@users.noreply.github.com>
2022-02-01 10:47:43 +0900

    Extract CSS and HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -10,12 +12,18 @@ module.exports = {
         rules: [{
             test: /\.css/,
             use: [{
-                    loader: 'style-loader',
+                    loader: MiniCssExtractPlugin.loader,
                 },
                 {
                     loader: 'css-loader',
                 }
             ]
         }]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
 }
